d027282d53fda018f2087185ef9d756567b8a8b3
Davide-Pedroncelli <84595751+Davide-Pedroncelli@users.noreply.github.com>
2022-03-07 12:07:47 +0100

    Add files via upload

---
+++ deepsleepLite/data_loader.py
@@ -0,0 +1,197 @@
+import os
+import numpy as np
+from deepsleepLite.utils import get_sequences, get_sequences_prob
+import random
+
+
+
+
+class DataLoader(object):
+
+    def __init__(self, data_dir, n_folds, fold_idx):
+        self.data_dir = data_dir
+        self.n_folds = n_folds
+        self.fold_idx = fold_idx
+
+    def _load_npz_file_DODO(self, npz_file, cond_prob):
+        """Load data and labels from a npz file."""
+        # print(f"{self.data_dir}/{npz_file}")
+        f = np.load(f"{self.data_dir}/{npz_file}", allow_pickle=True)
+        data = f["x"]
+        labels = f["y"]
+        if cond_prob:
+            labels_smoothed = f["y_smoothed"]
+        sampling_rate = int(f["fs"])
+        return data, labels, labels_smoothed, sampling_rate
+
+    def _load_npz_list_files_DODO(self, npz_files):
+        """Load data and labels from list of npz files."""
+        data = []
+        labels = []
+        labels_smoothed = []
+        sampling_rate = 128  # fs del database IS-RC
+        for npz_f in npz_files:
+            print(f"Loading {npz_f} ...")
+            tmp_data, tmp_labels, tmp_labels_smoothed, sampling_rate = self._load_npz_file_DODO(npz_f)
+
+            # Reshape the data to match the input of the model - conv2d
+            tmp_data = np.squeeze(tmp_data)
+            tmp_data = tmp_data[:, :, np.newaxis, np.newaxis]
+
+            # Casting
+            tmp_data = tmp_data.astype(np.float32)
+            tmp_labels = tmp_labels.astype(np.int32)
+            tmp_labels_smoothed = tmp_labels_smoothed.astype(np.float32)
+
+            data.append(tmp_data)
+            labels.append(tmp_labels)
+            labels_smoothed.append(tmp_labels_smoothed)
+
+        return data, labels, labels_smoothed, sampling_rate
+
+    def load_files_cv(self, dodh=False, dodo=False, dodo_dodh=False):
+
+        if dodh:
+            n_valid = 6
+        elif dodo:
+            n_valid = 12
+            test = ['Opsg_0_.npz', 'Opsg_7_.npz', 'Opsg_13_.npz', 'Opsg_20_.npz', 'Opsg_27_.npz']
+        elif dodo_dodh:
+            n_valid = 16
+
+        allfiles = os.listdir(self.data_dir)
+        allfiles.sort(key=lambda x: int(x.split("_")[1]))
+        # Database
+        if self.n_folds > len(allfiles):
+            raise Exception(" k-fold > n_subject!!")
+        else:
+            n_test = round(len(allfiles) / self.n_folds)  # number of subject in validation
+            test_idx_end = (self.fold_idx + 1) * n_test
+            test_idx_start = self.fold_idx * n_test
+
+            # Splitting files:
+            test_files = allfiles[test_idx_start:test_idx_end]
+            train_files = list(set(allfiles) - set(test_files))
+            valid_files = random.sample(train_files, n_valid)  # 20% del train è scelto come valid
+            train_files = list(set(train_files) - set(valid_files))
+
+            if dodo and self.fold_idx <=4:
+                test_files = list(set(test_files) - set([test[self.fold_idx]]))
+            if dodo and self.fold_idx == 9:
+                test_files.extend(test)
+
+            # Sorting:
+            test_files.sort(key=lambda x: int(x.split("_")[1]))
+            valid_files.sort(key=lambda x: int(x.split("_")[1]))
+            train_files.sort(key=lambda x: int(x.split("_")[1]))
+
+        return train_files, valid_files, test_files
+
+
+    def _load_npz_file_ISRC(self, npz_file, cond_prob): # cond_prob = empirical distribution
+        """Load data and labels from a npz file."""
+        cdir = os.getcwd()
+        os.chdir(self.data_dir)
+        with np.load(npz_file, allow_pickle=True) as f:
+            data = f["x"]  # single channel EEG 'C4-A1'
+            labels = f["y"]
+            sampling_rate = int(f["fs"])
+            if cond_prob:
+                labels_smoothed = f["y_smoothed"]
+        os.chdir(cdir)
+        return data, labels, labels_smoothed, sampling_rate
+
+    def _load_npz_list_files(self, npz_files, cond_prob):
+        """Load data and labels from list of npz files."""
+        data = []
+        labels = []
+        labels_smoothed = []
+        fs = None
+        for npz_f in npz_files:
+            print(f"Loading {npz_f} ...")
+            # TODO condition on database to be loaded
+            tmp_data, tmp_labels, tmp_labels_smoothed, sampling_rate = self._load_npz_file_DODO(npz_f, cond_prob)
+            if fs is None:
+                fs = sampling_rate
+            elif fs != sampling_rate:
+                raise Exception("Found mismatch in sampling rate.")
+
+            # Reshape the data to match the input of the model - conv2d
+            tmp_data = np.squeeze(tmp_data)
+            tmp_data = tmp_data[:, :, np.newaxis, np.newaxis]
+
+            # Casting
+            tmp_data = tmp_data.astype(np.float32)
+            tmp_labels = tmp_labels.astype(np.int32)
+            tmp_labels_smoothed = tmp_labels_smoothed.astype(np.float32)
+
+            data.append(tmp_data)
+            labels.append(tmp_labels)
+            labels_smoothed.append(tmp_labels_smoothed)
+
+        return data, labels, labels_smoothed, sampling_rate
+
+    def load_data_sequences(self, input_files, seq_length, train, cond_prob):
+
+        subject_files = input_files
+        subject_files.sort(key=lambda x: int(x.split("_")[1]))
+
+        # Load training set
+        if train is True:
+            # Load training set
+            print(f"\n========== [Fold-{self.fold_idx}] ==========\n")
+            print("Load training set:")
+        else:
+            # Load Validation set
+            print(f"\n========== [Fold-{self.fold_idx}] ==========\n")
+            print("Load validation set:")
+
+        data, label, labels_smoothed, sampling_rate = self._load_npz_list_files(npz_files=subject_files,
+                                                                                cond_prob=cond_prob)
+        # Extract sequences of length L=seq_length
+        data, label = get_sequences(
+            x=data, y=label, seq_length=seq_length
+        )
+        if cond_prob is True:
+            labels_smoothed = get_sequences_prob(
+                y=labels_smoothed, seq_length=seq_length
+            )
+            labels_smoothed = np.vstack(labels_smoothed)
+        if train is True:
+            print("Training set: n_psg={}".format(len(data)))
+        else:
+            print("Validation set: n_psg={}".format(len(data)))
+
+        data = np.vstack(
+            data)  # unisco tutte le epoche per righe, prima di questa operazione tutti i pazienti erano suddivisi ognuno con le sue epoche
+        label = np.vstack(label)
+
+        print(f"Number of examples = {len(data)}")
+
+        print(" ")
+
+        return data, label, sampling_rate, labels_smoothed
+
+    def load_ISRC_testdata_cv(self, data_dir):
+        """Load test data and labels in prediction.py from test list."""
+
+        f = np.load(data_dir)
+        test_files = f["test_files"].tolist()
+        data = []
+        labels = []
+        labels_smoothed = []
+        for file in test_files:
+            x, y, y_smoothed, sampling_rate = self._load_npz_file_ISRC(file,cond_prob=True)
+
+            # Reshape the data to match the input of the model - conv2d
+            x = np.squeeze(x)
+            x = x[:, :, np.newaxis, np.newaxis]
+
+            # Casting
+            x = x.astype(np.float32)
+            data.append(x)
+            labels.append(y)
+            labels_smoothed.append(y_smoothed)
+
+        return data, labels, labels_smoothed, test_files
+
---
+++ deepsleepLite/model.py
@@ -0,0 +1,267 @@
+import tensorflow.compat.v1 as tf
+
+from deepsleepLite.nn import *
+
+from deepsleepLite.sleep_stages import (NUM_CLASSES,
+                                        EPOCH_SEC_LEN,
+                                        SEQ_OF_EPOCHS,
+                                        SAMPLING_RATE)
+
+class SleepNetLite(object):
+
+    def __init__(
+            self,
+            batch_size,
+            input_dims,
+            seq_length,
+            n_classes,
+            is_train,
+            reuse_params,
+            use_MC_dropout,
+            freeze,
+            name="sleepnetlite"
+    ):
+        self.batch_size = batch_size
+        self.input_dims = input_dims
+        self.seq_length = seq_length
+        self.n_classes = n_classes
+        self.is_train = is_train
+        self.reuse_params = reuse_params
+        self.use_MC_dropout = use_MC_dropout
+        self.freeze = freeze
+        self.name = name
+
+        self.activations = []
+        self.layer_idx = 1
+        self.monitor_vars = []
+
+    #Un placeholder è un'inizializzazione di di una variabile nel graph, viene creata in dimensioni prima che essa venga riempita dai veri e propri dati
+    def _build_placeholder(self):
+        # Place holder del vettore di Input:
+        # name = nome della variabile che lo riempirà
+        name = "x_train" if self.is_train else "x_valid"
+        self.input_var = tf.compat.v1.placeholder(
+            tf.float32,
+            shape=(self.batch_size, int(self.input_dims*self.seq_length), 1, 1),#voleva un int e non un float
+            name= name + "_inputs"
+        )
+
+        # Target
+        self.target_var = tf.compat.v1.placeholder(
+            tf.int32,
+            shape=[self.batch_size, ],
+            name=name + "_targets"
+        )
+
+        # Target conditioned prob
+        self.target_var_conditioned = tf.compat.v1.placeholder(
+            tf.float32,
+            shape=[self.batch_size, 5],
+            name=name + "_targets_conditioned"
+        )
+        # Label smoothing alfa parameter
+        self.alfa = tf.compat.v1.placeholder(
+            tf.float32,
+            shape=None,
+            name=name + "_alfa"
+        )
+
+        # Conditional distributions on sleep stages
+        self.conditional_distribution = tf.compat.v1.placeholder(
+            tf.float32,
+            shape=[self.batch_size, self.n_classes],
+            name=name + "_smoothing_distribution"
+        )
+
+        # Conditional training
+        self.id_cluster = tf.compat.v1.placeholder(
+            tf.int32,
+            shape=[self.batch_size, ],
+            name=name + "_condition_encoding"
+        )
+
+    def _conv1d_layer(self, input_var, filter_size, n_filters, stride, wd=0, freeze=None):
+        input_shape = input_var.get_shape()
+        n_batches = input_shape[0]
+        input_dims = input_shape[1]
+        n_in_filters = input_shape[3]
+        name = f"l{self.layer_idx}_conv"
+        with tf.compat.v1.variable_scope(name) as scope:
+            output, weights = conv_1d(name="conv1d", input_var=input_var,
+                                      filter_shape=[filter_size, 1, n_in_filters, n_filters], stride=stride, bias=None,
+                                      wd=wd, freeze_layer=freeze)
+
+            output = batch_norm(name="bn", input_var=output, is_train=self.is_train, freeze_layer=freeze)
+
+            output = tf.nn.relu(output, name="relu")
+
+        self.activations.append((name, output))
+        self.layer_idx += 1
+        return output
+
+    def build_model(self, input_var):
+        # List to store the output of each CNNs
+        output_conns = []
+
+        ######### CNNs with small filter size at the first layer #########
+
+        # Convolution
+        network = self._conv1d_layer(input_var=input_var, filter_size=round(SAMPLING_RATE / 2), n_filters=64,
+                                     stride=round(SAMPLING_RATE / 16), wd=1e-3, freeze=self.freeze)
+
+        # Max pooling
+        name = "l{}_pool".format(self.layer_idx)
+        network = max_pool_1d(name=name, input_var=network, pool_size=8, stride=8)
+        self.activations.append((name, network))
+        self.layer_idx += 1
+
+        # Dropout
+        name = "l{}_dropout".format(self.layer_idx)
+        if self.is_train and not self.freeze:
+            network = tf.nn.dropout(network, rate = 1 - 0.5, name=name)
+        elif self.use_MC_dropout:
+            network = tf.nn.dropout(network, rate=1 - 0.5, name=name)
+        else:
+            network = tf.nn.dropout(network, rate = 0, name=name)
+        self.activations.append((name, network))
+        self.layer_idx += 1
+
+        # Convolution
+        network = self._conv1d_layer(input_var=network, filter_size=8, n_filters=128, stride=1, freeze=self.freeze)
+        network = self._conv1d_layer(input_var=network, filter_size=8, n_filters=128, stride=1, freeze=self.freeze)
+        network = self._conv1d_layer(input_var=network, filter_size=8, n_filters=128, stride=1, freeze=self.freeze)
+
+        # Max pooling
+        name = f"l{self.layer_idx}_pool"
+        network = max_pool_1d(name=name, input_var=network, pool_size=4, stride=4)
+        self.activations.append((name, network))
+        self.layer_idx += 1
+
+        # Flatten
+        name = "l{}_flat".format(self.layer_idx)
+        network = flatten(name=name, input_var=network)
+        self.activations.append((name, network))
+        self.layer_idx += 1
+
+        output_conns.append(network)
+
+        ######### CNNs with large filter size at the first layer #########
+
+        # Convolution
+        # network = self._conv1d_layer(input_var=input_var, filter_size=1024, n_filters=64, stride=128)
+        network = self._conv1d_layer(input_var=input_var, filter_size=round(SAMPLING_RATE * 4), n_filters=64,
+                                     stride=round(SAMPLING_RATE / 2), wd=1e-3, freeze=self.freeze)
+
+        # Max pooling
+        name = "l{}_pool".format(self.layer_idx)
+        network = max_pool_1d(name=name, input_var=network, pool_size=4, stride=4)
+        self.activations.append((name, network))
+        self.layer_idx += 1
+
+        # Dropout
+        name = "l{}_dropout".format(self.layer_idx)
+        if self.is_train and not self.freeze:
+            network = tf.nn.dropout(network, rate = 1 - 0.5, name=name)
+        elif self.use_MC_dropout:
+            network = tf.nn.dropout(network, rate=1 - 0.5, name=name)
+        else:
+            network = tf.nn.dropout(network, rate = 0, name=name)
+        self.activations.append((name, network))
+        self.layer_idx += 1
+
+        # Convolution
+        network = self._conv1d_layer(input_var=network, filter_size=6, n_filters=128, stride=1, freeze=self.freeze)
+        network = self._conv1d_layer(input_var=network, filter_size=6, n_filters=128, stride=1, freeze=self.freeze)
+        network = self._conv1d_layer(input_var=network, filter_size=6, n_filters=128, stride=1, freeze=self.freeze)
+
+        # Max pooling
+        name = "l{}_pool".format(self.layer_idx)
+        network = max_pool_1d(name=name, input_var=network, pool_size=2, stride=2)
+        self.activations.append((name, network))
+        self.layer_idx += 1
+
+        # Flatten
+        name = "l{}_flat".format(self.layer_idx)
+        network = flatten(name=name, input_var=network)
+        self.activations.append((name, network))
+        self.layer_idx += 1
+
+        output_conns.append(network)
+
+        ######### Aggregate and link two CNNs #########
+
+        # Concat
+        name = "l{}_concat".format(self.layer_idx)
+        network = tf.concat(output_conns, 1, name=name)  ##CHANGE new version tensorflow  (1, output_conns, name=name)
+        self.activations.append((name, network))
+        self.layer_idx += 1
+
+        # Dropout
+        name = "l{}_dropout".format(self.layer_idx)
+        if self.is_train and not self.freeze:
+            network = tf.nn.dropout(network, rate=1 - 0.5, name=name)
+        elif self.use_MC_dropout:
+            network = tf.nn.dropout(network, rate=1 - 0.5, name=name)
+        else:
+            network = tf.nn.dropout(network, rate=0, name=name)
+        self.activations.append((name, network))
+        self.layer_idx += 1
+
+        return network
+
+    def init_ops(self):
+        self._build_placeholder()
+
+        # Get loss and prediction operations
+        with tf.compat.v1.variable_scope(self.name) as scope:
+
+            # Reuse variables for validation
+            if self.reuse_params:
+                scope.reuse_variables()
+
+            # Build model
+            network = self.build_model(input_var=self.input_var)
+
+            # Softmax linear
+            name = f"l{self.layer_idx}_softmax_linear"
+            network = fc(name=name, input_var=network, n_hiddens=self.n_classes, bias=0.0, wd=0)
+            self.activations.append((name, network))
+            self.layer_idx += 1
+
+            # Outputs of softmax linear are logits
+            self.logits = network
+
+            # Uniform Label smoothing
+            name = "l{}_label_smoothing".format(self.layer_idx)
+            self.target_var_smoothed = label_smoothing_uniform(name=name, target_var=self.target_var, alfa=self.alfa, K=self.n_classes)
+
+            # Empirical Label smoothing
+            # name = "l{}_label_smoothing_conditioned".format(self.layer_idx)
+            # self.target_var_smoothed = label_smoothing_empirical(name=name, target_var=self.target_var, target_var_conditioned=self.target_var_conditioned, alfa=self.alfa)
+
+
+
+            ######### Compute loss #########
+
+            # Cross-entropy loss
+            loss = tf.nn.softmax_cross_entropy_with_logits(
+                labels=self.target_var_smoothed,
+                logits=self.logits,
+                name="softmax_cross_entropy_with_logits"
+
+            )  ##CHANGE : self.logits, self.target_var, name="sparse_softmax_cross_entropy_with_logits"
+
+            loss = tf.reduce_mean(loss, name="cross_entropy")
+
+            # Regularization loss
+            regular_loss = tf.add_n(
+                tf.compat.v1.get_collection("losses", scope=scope.name + "\/"),
+                name="regular_loss"
+            )
+
+            # Total loss
+            self.loss_op = tf.add(loss, regular_loss)
+
+            # Predictions
+            self.pred_op = tf.argmax(self.logits, 1)
+
