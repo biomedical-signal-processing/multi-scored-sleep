0eda5c46d18776f020c6e20ab5a3305b34ba285e
Davide-Pedroncelli <84595751+Davide-Pedroncelli@users.noreply.github.com>
2022-03-18 19:52:27 +0100

    Update Hypnodensity_Graphs.py

--- Hypnodensity_Graphs.py
+++ Hypnodensity_Graphs.py
@@ -1,13 +1,6 @@
 import numpy as np
 import os
-# import matplotlib.pyplot as plt
-# import pandas as pd
-# import matplotlib.image as mpimg
-# # import sklearn.metrics.pairwise as s
-# from sklearn.metrics import accuracy_score, confusion_matrix, cohen_kappa_score, f1_score
-# from scipy import stats
-# import seaborn as sns
-# from statsmodels.stats.diagnostic import lilliefors
+
 def cos_sim(p,q):
     return np.dot(p,q)/(np.linalg.norm(p)*np.linalg.norm(q))
 
@@ -58,7 +51,7 @@ def hypnodensity(L, path, hypno_sc):
 
 if __name__ == '__main__':
     # Compute cos similarity across all alpha smoothed:
-    base_path = "C:/Users\pedro\OneDrive - Politecnico di Torino\Desktop\DSN-L\pre_processing\DOD-O_V2/"
+    base_path = "/content/drive/MyDrive/Experiment _Paper/DOD-O_V2/"    
     files = sorted(os.listdir(base_path))
     test = ['Opsg_0_.npz', 'Opsg_7_.npz', 'Opsg_13_.npz', 'Opsg_20_.npz', 'Opsg_27_.npz','Opsg_54_.npz']
     files = list(set(files) - set(test))
@@ -74,141 +67,19 @@ if __name__ == '__main__':
         hypno_sc.append(hypno[1:len(hypno)-1,:])
 
     #compute COSINE-SIMILARITY
-    path_base = "C:/Users\pedro\OneDrive - Politecnico di Torino\Desktop\DSN-L/prediction/base"
-    path_lsu = "C:/Users\pedro\OneDrive - Politecnico di Torino\Desktop\DSN-L/prediction/LSU"
-    path_lse = "C:/Users\pedro\OneDrive - Politecnico di Torino\Desktop\DSN-L/prediction/LSE"
+    base_path_LSE = "/content/drive/MyDrive/Experiment _Paper/DSNL/DODO/prediction/LSE"
+    base_path_LSU = "/content/drive/MyDrive/Experiment _Paper/DSNL/DODO/prediction/LSU"
+    base_path_base = "/content/drive/MyDrive/Experiment _Paper/DSNL/DODO/prediction/base"
     L = [0]
-    cos_base, std_base = hypnodensity(L, path_base, hypno_sc)
+    cos_base, std_base = hypnodensity(L, base_path_base, hypno_sc)
     L = [0.1, 0.2, 0.3, 0.4, 0.5]
-    cos_lsu, std_lsu = hypnodensity(L, path_lsu, hypno_sc)
+    cos_lsu, std_lsu = hypnodensity(L, base_path_LSU, hypno_sc)
     L = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-    cos_lse, std_lse = hypnodensity(L, path_lse, hypno_sc)
+    cos_lse, std_lse = hypnodensity(L, base_path_LSE, hypno_sc)
 
-
-    plt.plot(cos_lsu,"-^",color="dimgrey")
-    plt.plot(cos_lse,"-^",color="navy")
-    # plt.errorbar(np.arange(9),cos_one[1:-1], std_one[1:-1], linestyle='None', marker='^')
-    # plt.errorbar(np.arange(10),cos_two[1:], std_two[1:], linestyle='None', marker='^')
-    plt.axhline(y=cos_base[0], color='deepskyblue', linestyle='-', linewidth="1")
-    plt.title("Cosine similarity across all α-value")
-    plt.xlabel("α")
-    plt.ylabel("Averaged Cosine-similarity")
-    plt.grid(linewidth=0.3)
-    plt.xticks(np.arange(11), ["0", "0.1", "0.2", "0.3", "0.4", "0.5", "0.6", "0.7", "0.8", "0.9", "1"])
-    # plt.legend(["cos_DSLone","cos_DSLtwo"])
-    plt.xticks(np.arange(10), ["0.1", "0.2", "0.3", "0.4", "0.5", "0.6", "0.7", "0.8", "0.9", "1"])
-    plt.show()
-
-
-
-
-#
-# import numpy as np
-# import os
-# import matplotlib.pyplot as plt
-# import matplotlib.image as mpimg
-# # import sklearn.metrics.pairwise as s
-# from sklearn.metrics import accuracy_score, confusion_matrix, cohen_kappa_score, f1_score
-# from scipy import stats
-# import seaborn as sns
-# from statsmodels.stats.diagnostic import lilliefors
-# def cos_sim(p,q):
-#     return np.dot(p,q)/(np.linalg.norm(p)*np.linalg.norm(q))
-#
-# def load_hypno(path):
-#     hypno = []
-#     for fold_idx in range(10):
-#         f1 = np.load(f"{path}/output_fold{fold_idx}.npz", allow_pickle=True)
-#         prob_pred = f1["prob_pred"]
-#         hypno.extend(prob_pred)
-#     return hypno
-# def compute_similarity(hypno_sc, hypno):
-#     cos = []
-#     cos_distr = []
-#     std = []
-#     for i in range(70):
-#         cos_ = []
-#         for n, epoch in enumerate(hypno[i]):  # for perchè il mio pc è lento
-#             cos_.append(cos_sim(epoch, hypno_sc[i][n, :]))
-#         cos.append(np.mean(cos_))
-#         std.append(np.std(cos_))
-#         cos_distr.append(cos_)
-#     return cos, cos_distr, std
-#
-# def hypnodensity(L, path, hypno_sc):
-#
-#     cos_l = []
-#     std_l = []
-#     for a in L:
-#         p = []
-#         hypno = []
-#         for fold_idx in range(10):
-#             f1 = np.load(f"{path}/{a}/output_fold{fold_idx}.npz",
-#                 allow_pickle=True)
-#             prob_pred = f1["prob_pred"]
-#             p.extend(prob_pred)
-#             hypno.append(p)
-#         for ii in range(len(L)):
-#             coss = []
-#             for i in range(55):
-#                 cos = []
-#                 for n, epoch in enumerate(hypno[ii][i]):  # for perchè il mio pc è lento
-#                   try:
-#                     cos.append(cos_sim(epoch, hypno_sc[i][n, :]))
-#                   except:
-#                     pass
-#                 coss.append(np.mean(cos))
-#         cos_l.append(np.mean(coss))
-#         std_l.append(np.std(coss))
-#
-#     return cos_l, std_l
-#
-# if __name__ == '__main__':
-#     # Compute cos similarity across all alpha smoothed:
-#     base_path = "/content/drive/MyDrive/Experiment _Paper/DOD-O_V2/"
-#     files = sorted(os.listdir(base_path))
-#     files.sort(key=lambda x: int(x.split("_")[1]))
-#     hypno_sc = []
-#     hypno_lse = []
-#     hypno_lsu = []
-#     hypno_base = []
-#     for file in files:
-#         f2 = np.load(base_path+file)
-#         hypno = f2["y_smoothed"]
-#         hypno_sc.append(hypno[1:len(hypno)-1,:])
-#
-#     #compute COSINE-SIMILARITY
-#     base_path_LSE = "/content/drive/MyDrive/Experiment _Paper/DSNL/DODO/prediction/LSE"
-#     base_path_LSU = "/content/drive/MyDrive/Experiment _Paper/DSNL/DODO/prediction/LSU"
-#     base_path_base = "/content/drive/MyDrive/Experiment _Paper/DSNL/DODO/prediction/base"
-#     L = [0]
-#     cos_base, std_base = hypnodensity(L, base_path_base, hypno_sc)
-#     L = [0.1, 0.2, 0.3, 0.4, 0.5]
-#     cos_lsu, std_lsu = hypnodensity(L, base_path_LSU, hypno_sc)
-#     L = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7]
-#     cos_lse, std_lse = hypnodensity(L, base_path_LSE, hypno_sc)
-#
-#     print(f"LSE:\n ACS:{np.round(cos_lse,3)}\n ±\n std:{np.round(std_lse,3)}\n")
-#     print(f"LSU:\n ACS:{np.round(cos_lsu,3)}\n ±\n std:{np.round(std_lsu,3)}\n")
-#     print(f"base:\n ACS:{np.round(cos_base,3)}\n ±\n std:{np.round(std_base,3)}\n")
-#
-#     plt.plot(cos_lsu,"-^",color="dimgrey")
-#     plt.plot(cos_lse,"-^",color="navy")
-#     # plt.errorbar(np.arange(9),cos_one[1:-1], std_one[1:-1], linestyle='None', marker='^')
-#     # plt.errorbar(np.arange(10),cos_two[1:], std_two[1:], linestyle='None', marker='^')
-#     plt.axhline(y=cos_base[0], color='deepskyblue', linestyle='-', linewidth="1")
-#     plt.title("Cosine similarity across all α-value")
-#     plt.xlabel("α")
-#     plt.ylabel("Averaged Cosine-similarity")
-#     plt.grid(linewidth=0.3)
-#     plt.xticks(np.arange(11), ["0", "0.1", "0.2", "0.3", "0.4", "0.5", "0.6", "0.7", "0.8", "0.9", "1"])
-#     # plt.legend(["cos_DSLone","cos_DSLtwo"])
-#     plt.xticks(np.arange(10), ["0.1", "0.2", "0.3", "0.4", "0.5", "0.6", "0.7", "0.8", "0.9", "1"])
-#     plt.show()
-#
-#
-#
-
+    print(f"LSE:\n ACS:{np.round(cos_lse,3)}\n ±\n std:{np.round(std_lse,3)}\n")
+    print(f"LSU:\n ACS:{np.round(cos_lsu,3)}\n ±\n std:{np.round(std_lsu,3)}\n")
+    print(f"base:\n ACS:{np.round(cos_base,3)}\n ±\n std:{np.round(std_base,3)}\n")
 
 
 
